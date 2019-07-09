97fded15c57254a05482b0a39456941cd134886e
Kono Shoichiro <shoichiro@Shoichiros-MacBook-Pro.local>
2019-07-09 23:48:39 +0900

    fix App.js

--- App.js
+++ App.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.appbar}>
+        <View>
+          <Text style={styles.appbarTitle}>MEMOT</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイデア</Text>
+          <Text style={styles.memoDate}>2019/07/16</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイデア</Text>
+          <Text style={styles.memoDate}>2019/07/16</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイデア</Text>
+          <Text style={styles.memoDate}>2019/07/16</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイデア</Text>
+          <Text style={styles.memoDate}>2019/07/16</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイデア</Text>
+          <Text style={styles.memoDate}>2019/07/16</Text>
+        </View>
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 }
@@ -15,5 +52,44 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
+  },
+
+  memoList: {
+    width: '100%',
+    flex: 1,
+  },
+
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
+  },
+
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 78,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
