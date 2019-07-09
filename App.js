3f5384821ccbbf8ed7d3b2570065d6fb461afe3e
Kono Shoichiro <shoichiro@Shoichiros-MacBook-Pro.local>
2019-07-10 00:44:01 +0900

    add CircleButton.js

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
 import MemoList from './src/components/MemoList';
+import CircleButton from './src/elements/CircleButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-
       <Appbar />
       <MemoList />
-
-      <View style={styles.memoAddButton}>
-        <Text style={styles.memoAddButtonText}>+</Text>
-      </View>
+      <CircleButton>+</CircleButton>
     </View>
   );
 }
@@ -25,27 +22,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 78,
-  },
-
-  memoAddButton: {
-    position: 'absolute',
-    bottom: 32,
-    right: 32,
-    width: 48,
-    height: 48,
-    backgroundColor: '#E31676',
-    borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.4,
-    shadowRadius: 3,
-  },
-
-  memoAddButtonText: {
-    fontSize: 32,
-    lineHeight: 34,
-    color: '#fff',
   },
 });
